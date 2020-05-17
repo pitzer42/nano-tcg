8e3c0b752d5a1cf31817224627330184261fd4f3
Arthur Pitzer <arthurpitzer@id.uff.br>
2020-05-16 21:22:20 -0300

    test mulligan

--- scripts/viz.sh
+++ scripts/viz.sh
@@ -1,1 +1,2 @@
 pydeps nano_magic --reverse --cluster --max-cluster-size=1 --keep-target-cluster --rmprefix nano_magic.
+pydeps ui --reverse --cluster --max-cluster-size=1 --keep-target-cluster --rmprefix ui.
--- tests/unit/nano_tcg/use_cases/test_initial_hand.py
+++ tests/unit/nano_tcg/use_cases/test_initial_hand.py
@@ -18,7 +18,7 @@ async def test_draw_initial_hand():
 
 
 @pytest.mark.asyncio
-async def test_mulligan_more_than_initial_hand_size_times():
+async def test_mulligan_initial_hand_size_times():
     client = AsyncMock()
     client.prompt_mulligan.side_effect = [True] * INITIAL_HAND_SIZE
     deck = list(range(60))
@@ -30,13 +30,12 @@ async def test_mulligan_more_than_initial_hand_size_times():
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip
-async def test_hand_size_zero_is_stop_condition():
+async def test_mulligan_more_initial_hand_size_times():
     client = AsyncMock()
-    deck = [1, 2, 3]
+    client.prompt_mulligan.side_effect = [True] * (INITIAL_HAND_SIZE + 1)
+    deck = list(range(60))
     original_deck = list(deck)
-    hand_size = 0
-    await draw_initial_hand(client, deck, hand_size)
-    client.send.assert_not_called()
-    client.receive.assert_not_called()
-    assert client.deck == original_deck
+    hand = await draw_initial_hand(client, deck)
+    assert len(hand) == 0
+    assert client.prompt_mulligan.await_count == INITIAL_HAND_SIZE
+    assert len(deck) == len(original_deck)
