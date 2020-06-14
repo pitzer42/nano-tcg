2ec1adb1f89ad7d888c52c0b87796bb07cb3631c
Arthur Pitzer <arthurpitzer@id.uff.br>
2020-06-14 14:50:32 -0300

    minor changes

--- scripts/viz.sh
+++ scripts/viz.sh
@@ -1,3 +1,3 @@
+pydeps tictactoe --reverse --cluster --max-cluster-size=1 --keep-target-cluster --rmprefix tictactoe.
 pydeps nano_magic --reverse --cluster --max-cluster-size=1 --keep-target-cluster --rmprefix nano_magic.
 pydeps ui --reverse --cluster --max-cluster-size=1 --keep-target-cluster --rmprefix ui.
-pydeps tictactoe --reverse --cluster --max-cluster-size=1 --keep-target-cluster --rmprefix tictactoe.
